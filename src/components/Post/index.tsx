77b76d10626afe997bbb4df55d71a68764c15852
IsaqueMansur <91393713+IsaqueMansur@users.noreply.github.com>
2022-09-12 14:28:27 -0300

    projeto react router dom v6

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,15 @@
+import { useParams, useSearchParams } from 'react-router-dom';
+import './style.css';
+
+export const Post = () => {
+    const params = useParams();
+    const { id } = params;
+    const [qs] = useSearchParams();
+
+    return (
+        <div>
+            {/* http://IP/posts/1000?page=10&segunda=segundo  */}
+            <h1>Post {`Param.: ${id}`} {`QS: ${qs.get('segunda')}`}</h1>
+        </div>
+    );
+};
---
+++ src/main.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Menu } from './components/Menu';
+import './styles/global.css';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Post } from './components/Post';
+import { Redirect } from './components/Redirect';
+import { NoteFound } from './components/NotFound';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Menu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/posts/:id' element={<Post />} />
+        <Route path='/posts' element={<Post />} />
+        <Route path='/redirect' element={<Redirect />} />
+        <Route path='*' element={<NoteFound />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
